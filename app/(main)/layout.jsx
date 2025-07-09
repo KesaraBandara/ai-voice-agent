67c94559dab24aca277705c8312bed9203dda7e9
Harsha <kesrabandara1998@gmail.com>
2025-07-09 23:46:29 +0530

    aded header

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -4,7 +4,7 @@ import AppHeader from './_components/AppHeader'
 function DashboardLayou({ children }) {
     return (
         <div>
-            <AppHeader />
+            <AppHeader/>
             <div className='p-10 mt-14 md:px-20 lg:px-32 xl:px-56 2xl:px-72'>
                 {children}
             </div>
