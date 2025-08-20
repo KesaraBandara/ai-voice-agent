f675b54062db5a943635c1928bc2e434614a0e91
Harsha <kesrabandara1998@gmail.com>
2025-08-20 18:09:25 +0530

    microphone enable and diconnected functione is done

--- app/(main)/discussion-room/[roomid]/page.jsx
+++ app/(main)/discussion-room/[roomid]/page.jsx
@@ -3,11 +3,14 @@ import { api } from "@/convex/_generated/api";
 import { CoachingExpert } from "@/services/Options";
 import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { UserButton } from "@stackframe/stack";
 import { Button } from "@/components/ui/button";
+import dynamic from "next/dynamic";
 import RecordRTC from "recordrtc";
+// const RecordRTC = dynamic(() => import("recordrtc"), { ssr: false });
+
 
 function DiscussionRoom() {
   const { roomid } = useParams();
@@ -17,6 +20,9 @@ function DiscussionRoom() {
   const [expert, setExpert] = useState();
   const [enableMic, setEnableMic] = useState(false);
   const recorder = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  let silenceTimeout;
 
   useEffect(() => {
     if (DiscussionRoomData) {
@@ -29,6 +35,7 @@ function DiscussionRoom() {
   }, [DiscussionRoomData]);
 
   const connectToServer = () => {
+    setEnableMic(true);
     if (typeof window !== "undefined" && typeof navigator !== "undefined") {
       navigator.mediaDevices
         .getUserMedia({ audio: true })
@@ -48,7 +55,7 @@ function DiscussionRoom() {
               // Reset the silence detection timer on audio input
               clearTimeout(silenceTimeout);
               const buffer = await blob.arrayBuffer();
-              //console.log(buffer)
+              console.log(buffer)
               // Restart the silence detection timer
               silenceTimeout = setTimeout(() => {
                 console.log("User stopped talking");
@@ -64,7 +71,12 @@ function DiscussionRoom() {
     }
   };
 
-  const disconnect = async (e) => {};
+  const disconnect = async (e) => {
+    e.preventDefault();
+    recorder.current.pauseRecording();
+    recorder.current = null;
+    setEnableMic(false);
+  };
   return (
     <div className="-mt-12">
       <h2 className="text-lg font-bold">
